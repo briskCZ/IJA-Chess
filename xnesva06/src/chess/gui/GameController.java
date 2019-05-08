e0caee1caa8f824520c8e4ddbf4beee96bf3e07d
briskCZ <briskcz@gmail.com>
2019-05-08 22:39:07 +0200

    Tring to make the chess board square

--- xnesva06/src/chess/gui/GameController.java
+++ xnesva06/src/chess/gui/GameController.java
@@ -4,17 +4,23 @@ import chess.board.Field;
 import chess.figures.Figure;
 import chess.figures.FigureType;
 import chess.game.Game;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.List;
@@ -36,6 +42,7 @@ public class GameController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+
         System.out.println(game.getGameId());
 
         listView.setOnMouseClicked(event -> ListClicked(listView.getSelectionModel().getSelectedItem()));
@@ -47,8 +54,9 @@ public class GameController implements Initializable {
         items.add("Four");
         items.add("Five");
 
-        //Image image = new Image(Main.class.getResource("test.png").toExternalForm(), 100, 100, true, true);
         SetupFigures();
+
+
     }
 
     private void SetupFigures()
@@ -64,6 +72,7 @@ public class GameController implements Initializable {
                 }else{
                     field = new GuiBoardField(x,y);
                 }
+
                 field.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
                 field.setOnAction(event -> FieldClicked(field));
                 chessBoardGridPane.add(field,x,y);
