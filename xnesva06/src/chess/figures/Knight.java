15943f46d069ce9ffaa563097aac23725093403a
briskCZ <briskcz@gmail.com>
2019-05-11 17:28:25 +0200

    added possible moves for knight

--- xnesva06/src/chess/figures/Knight.java
+++ xnesva06/src/chess/figures/Knight.java
@@ -20,6 +20,36 @@ public class Knight extends Figure
     @Override
     public ArrayList<Field> getPossibleMoveFields(ChessBoard board)
     {
-        return new ArrayList<>();
+        ArrayList<Field> possibleMoveFields = new ArrayList<>();
+
+        checkMove(board,possibleMoveFields,row + 2, column + 1);
+        checkMove(board,possibleMoveFields,row + 2, column - 1);
+        checkMove(board,possibleMoveFields,row - 2, column + 1);
+        checkMove(board,possibleMoveFields,row - 2, column - 1);
+        checkMove(board,possibleMoveFields,row + 1, column + 2);
+        checkMove(board,possibleMoveFields,row + 1, column - 2);
+        checkMove(board,possibleMoveFields,row - 1, column + 2);
+        checkMove(board,possibleMoveFields,row - 1, column - 2);
+
+        return  possibleMoveFields;
+    }
+
+    private boolean checkMove(ChessBoard board, ArrayList<Field> possibleArrayMoves, int row, int column)
+    {
+        Field f = board.getField(row, column);
+        if(f != null){
+            if (f.isOccupiedWithEnemyFig(this))
+            {
+                possibleArrayMoves.add(f);
+                return true;
+            }
+            if (f.isOccupied())
+            {
+                return true;
+            }
+            possibleArrayMoves.add(f);
+            return false;
+        }
+        return false;
     }
 }
