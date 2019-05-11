3b3881983d6400e8547fa774342c37511cc0b632
briskCZ <briskcz@gmail.com>
2019-05-11 12:55:23 +0200

    figures are now represented by images

--- xnesva06/src/chess/gui/GuiBoardField.java
+++ xnesva06/src/chess/gui/GuiBoardField.java
@@ -1,15 +1,17 @@
 package chess.gui;
 
 import chess.figures.Figure;
+import chess.figures.FigureColor;
 import chess.figures.FigureType;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+
 
 public class GuiBoardField extends Button {
 
@@ -25,7 +27,8 @@ public class GuiBoardField extends Button {
     private String base_style = "-fx-font-size: 50px; -fx-background-radius: 0; -fx-padding: 0;  ";
     private String current_style = "";
 
-    Image figure_image ;
+    private File image_file;
+    private Image figure_image ;
 
     GuiBoardField(int col, int row){
         this.col = col;
@@ -39,16 +42,61 @@ public class GuiBoardField extends Button {
         this.figure = figure;
         this.setText(figure.toString());
 
-        /*switch (this.figure.getType()){
-            case Pawn:
-                //figure_image = new Image(getClass().getResourceAsStream("xnesva06/lib/pawn_w.png"));
-                //figure_image = new Image(Main.class.getResource("xnesva06/lib/pawn_w.png").toExternalForm(), 100, 100, true, true);
-                break;
+        if(this.figure.getColor() == FigureColor.White){
+            switch (this.figure.getType()){
+                case Bishop:
+                    image_file = new File("xnesva06/lib/bishop_w.png");
+                    break;
+                case King:
+                    image_file = new File("xnesva06/lib/king_w.png");
+                    break;
+                case Knight:
+                    image_file = new File("xnesva06/lib/knight_w.png");
+                    break;
+                case Pawn:
+                    image_file = new File("xnesva06/lib/pawn_w.png");
+                    break;
+                case Queen:
+                    image_file = new File("xnesva06/lib/queen_w.png");
+                    break;
+                case Rook:
+                    image_file = new File("xnesva06/lib/tower_w.png");
+                    break;
+            }
+        }else{
+                switch (this.figure.getType()){
+                    case Bishop:
+                        image_file = new File("xnesva06/lib/bishop_b.png");
+                        break;
+                    case King:
+                        image_file = new File("xnesva06/lib/king_b.png");
+                        break;
+                    case Knight:
+                        image_file = new File("xnesva06/lib/knight_b.png");
+                        break;
+                    case Pawn:
+                        image_file = new File("xnesva06/lib/pawn_b.png");
+                        break;
+                    case Queen:
+                        image_file = new File("xnesva06/lib/queen_b.png");
+                        break;
+                    case Rook:
+                        image_file = new File("xnesva06/lib/tower_b.png");
+                        break;
+                }
+        }
 
+        try {
+            figure_image = SwingFXUtils.toFXImage(ImageIO.read(image_file), null );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        }*/
-
-        //this.setGraphic(new ImageView(figure_image));
+        ImageView imageView = new ImageView(figure_image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(GameController.field_size - 10);
+        imageView.setFitWidth(GameController.field_size - 10);
+        this.setGraphic(imageView);
 
         this.contextMenu = contextMenu;
         setBaseStyle();
@@ -84,16 +132,16 @@ public class GuiBoardField extends Button {
         }
     }
 
-    public void setEnabled(boolean enabled){
+    void setEnabled(boolean enabled){
         this.isEnabled = enabled;
         updateProperties();
     }
 
-    public boolean getEnabled(){
+    boolean getEnabled(){
         return this.isEnabled;
     }
 
-    public int getCol() {
+    int getCol() {
         return col;
     }
 
